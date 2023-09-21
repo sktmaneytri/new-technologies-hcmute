286a5c64ad2e6fc452f4b0072bda9f4ff4e9fe4e
sktmaneytri <sktmaneytri@.com>
2023-09-21 14:47:59 +0700

    last commit for week04

--- 20110422/week04/index.js
+++ 20110422/week04/index.js
@@ -1,6 +1,15 @@
+/*
+* I suggest that you can use Postman to access the APIs
+* APIs document:
+* to get all students are in "mygroup" : GET http://localhost:5000/ || GET http://localhost:5000/message
+* to add a new students to "mygroup": POST http://localhost:5000/MSSV/<id> & A json object {"id": "NEW_ID", "name": "name of new student"}
+* to retrieve information about a student in the "mygroup" based on the provided <id>:  GET http://localhost:5000/MSSV/<id>
+* to retrieve a name of student by their id : GET http://localhost:5000/message/<id>
+* Done by: Minh Tri Nguyen | 20110422
+*/
 const express = require("express");
 const bodyParser = require("body-parser");
-const mainRouter = require("./week04/routes/index");
+const mainRouter = require("./routes/index");
 
 const app = express();
 const port = 5000;
